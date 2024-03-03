71f401bf908f5bec6188169a518e5f9d23bb727d
小李 <1471363285@qq.com>
2024-03-03 23:08:26 +0800

    尝试添加测试文件

---
+++ test/index.spec.ts
@@ -0,0 +1,15 @@
+// import { asyncPool } from "../src/main.ts";
+
+// console.log(asyncPool);
+
+describe("asyncpool-js", () => {
+  it("test limit params", () => {});
+  it("test requestParams params", () => {});
+  it("test useAllSettled params", () => {});
+});
+
+describe("test", () => {
+  it("add", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
